2349947acb7d092b07c2bd01d4e2cf374d6df04a
darkshadowrule <prajjwaldxi3@gmail.com>
2019-02-21 23:12:23 +0530

    Added autoscrolling feauture

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,24 @@
 var socket=io();
+function scrollToBottom() {
+   var message=jQuery('#messages');
+   var newMessage=message.children('li:last-child')
+   var clientHeight=message.prop('clientHeight');
+    var scrollTop=message.prop('scrollTop');
+     var scrollHeight=message.prop('scrollHeight');
+     var newMessageHeight=newMessage.innerHeight();
+     var lastMessageHeight=newMessage.prev().innerHeight();
+     if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight)
+     {
+       message.scrollTop(scrollHeight);
+     }
+}
+
 socket.on('connect',function(){
 console.log("Connected to server")
 })
-
+socket.on('disconnect',function(){
+console.log("Disconnected from server")
+})
 socket.on('newMessage',function(data){
   var formattedTime=moment(data.createdAt).format('h:mm a');
   var template=jQuery('#message-template').html();
@@ -12,12 +28,9 @@ socket.on('newMessage',function(data){
     createdAt:formattedTime
   });
   jQuery('#messages').append(html)
+  scrollToBottom();
 })
 
-
-socket.on('disconnect',function(){
-console.log("Disconnected from server")
-})
 
 socket.on('newLocationMessage',function (message) {
   var formattedTime=moment(message.createdAt).format('h:mm a');
@@ -28,6 +41,7 @@ socket.on('newLocationMessage',function (message) {
     createdAt:formattedTime
   });
   jQuery("#messages").append(html);
+  scrollToBottom();
 })
 jQuery('#message-form').on('submit',function (e) {
   e.preventDefault();
